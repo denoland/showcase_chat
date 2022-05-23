d142d8fb7f587efece75ab617e972b9dc20a7943
crowlkats <crowlkats@toaxl.com>
2022-05-23 10:40:00 +0200

    pin fresh and add deno.json

--- client_deps.ts
+++ client_deps.ts
@@ -1,6 +1,6 @@
-export * from "https://raw.githubusercontent.com/lucacasonato/fresh/main/runtime.ts";
+export * from "https://raw.githubusercontent.com/lucacasonato/fresh/59e6398d7724f4990aa3ace12dce36e11b20e1ba/runtime.ts";
 // Setup twind
-import { IS_BROWSER } from "https://raw.githubusercontent.com/lucacasonato/fresh/main/runtime.ts";
+import { IS_BROWSER } from "https://raw.githubusercontent.com/lucacasonato/fresh/59e6398d7724f4990aa3ace12dce36e11b20e1ba/runtime.ts";
 import { apply, setup, tw } from "https://esm.sh/twind@0.16.16";
 export { apply, setup, tw };
 if (IS_BROWSER) {
--- server_deps.ts
+++ server_deps.ts
@@ -1,5 +1,5 @@
 import "https://deno.land/std@0.134.0/dotenv/load.ts";
-export * from "https://raw.githubusercontent.com/lucacasonato/fresh/main/server.ts";
+export * from "https://raw.githubusercontent.com/lucacasonato/fresh/59e6398d7724f4990aa3ace12dce36e11b20e1ba/server.ts";
 export { virtualSheet } from "https://esm.sh/twind@0.16.16/sheets";
 export {
   deleteCookie,

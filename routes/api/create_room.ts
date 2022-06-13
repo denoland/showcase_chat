33e4c75a1ff3b927731ab0d23f59d993142a81ed
David Sherret <dsherret@gmail.com>
2022-06-13 14:49:02 -0400

    feat: bad words list filter room name
    Closes #38

--- routes/api/create_room.ts
+++ routes/api/create_room.ts
@@ -1,9 +1,11 @@
 import { HandlerContext, Handlers } from "../../server_deps.ts";
 import { database } from "../../communication/database.ts";
+import { badWordsCleanerLoader } from "../../helpers/bad_words.ts";
 
 export const handler: Handlers = {
   POST: async (req: Request, _ctx: HandlerContext): Promise<Response> => {
-    const name = await req.text();
+    const badWordsCleaner = await badWordsCleanerLoader.getInstance();
+    const name = badWordsCleaner.clean(await req.text());
     const roomId = await database.ensureRoom(name);
 
     return new Response(roomId, {

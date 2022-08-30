f57b2f562d99a02e9a037cea7821bf6d83a490fc
hashrock <hashrock@users.noreply.github.com>
2022-08-30 18:21:28 +0900

    footer: add fresh SVG badge (#59)

--- helpers/Footer.tsx
+++ helpers/Footer.tsx
@@ -4,11 +4,18 @@ import { tw } from "@twind";
 export function Footer() {
   return (
     <div
-      class={tw
-        `mt-16 flex justify-center items-center flex-col border-none bg-transparent text-black`}
+      class={tw`mt-16 flex justify-center items-center border-none bg-transparent text-black gap-4`}
     >
+      <a href="https://fresh.deno.dev">
+        <img
+          width="197"
+          height="37"
+          src="https://fresh.deno.dev/fresh-badge.svg"
+        />
+      </a>
+
       <a
-        class={tw`font-bold flex justify-start items-center cursor-pointer`}
+        class={tw`font-bold flex justify-start items-center cursor-pointer gap-1 hover:underline focus:underline`}
         href="https://github.com/denoland/showcase_chat"
         rel="noopener noreferrer"
         target="_blank"
